72ed94f95a28cc0bc15fabda8aa5b3cace710724
Ivan583 <ivanych2293@gmail.com>
2020-10-16 10:37:39 +0300

    Автовозврат к списку товаров

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,10 @@ const routes = [{
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "form" */ '../views/Form.vue')
+  },
+  {
+    path: '*',
+    redirect: '/'
   }
 ]
 
