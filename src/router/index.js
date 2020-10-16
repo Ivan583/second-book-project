a387e3b6caa0c419d0eec3167a8beaf75ffb8bc4
Ivan583 <ivanych2293@gmail.com>
2020-10-16 10:56:53 +0300

    Добавление компонента Form

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,12 @@ const routes = [{
     component: Main
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/form',
+    name: 'Form',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "form" */ '../views/Form.vue')
   }
 ]
 
